ca685e26c72476475526dcc5171c19ebd07d72d8
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-06-20 18:21:28 +0900

    Add files via upload

--- js/project07.js
+++ js/project07.js
@@ -2,5 +2,40 @@ $(function () {
   $(".main_slide").slick({
     arrows: false,
     autoplay: true,
+    autoplaySpeed: 3500,
   });
-});
+
+  $('.main_visual .menu li').on('click', function (e) {
+    e.preventDefault();
+    const idx = $(this).index(); //
+    $('.main_slide').slick('slickGoTo', idx);
+  });
+
+  $('.main_slide').on('init afterChange', function (e, s, c) {
+    const current = $('.main_slide .slick-current');
+    current.addClass('on').siblings().removeClass('on');
+
+    $('.main_visual .menu li').eq(0).addClass('on');
+    $('.main_visual .menu li').eq(c).addClass('on')
+      .siblings().removeClass('on');
+  });
+
+  // $(".con_slide").slick({
+  //   arrows: false,
+  //   centerMode: true,
+  //   centerPadding: '100px',
+  //   slidesToShow: 1,
+  //   // variableWidth: true
+  // });
+
+  $(".con_slide").slick({
+    dots: true,
+    draggable: false,
+    arrows: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: '350px',
+  });
+
+
+})
