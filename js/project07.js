f6454c521dd24a00982525d18a632ca123799a17
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-06-21 01:33:57 +0900

    Add files via upload

--- js/project07.js
+++ js/project07.js
@@ -5,19 +5,22 @@ $(function () {
     autoplaySpeed: 3500,
   });
 
-  $('.main_visual .menu li').on('click', function (e) {
+  $(".main_visual .menu li").on("click", function (e) {
     e.preventDefault();
     const idx = $(this).index(); //
-    $('.main_slide').slick('slickGoTo', idx);
+    $(".main_slide").slick("slickGoTo", idx);
   });
 
-  $('.main_slide').on('init afterChange', function (e, s, c) {
-    const current = $('.main_slide .slick-current');
-    current.addClass('on').siblings().removeClass('on');
+  $(".main_slide").on("init afterChange", function (e, s, c) {
+    const current = $(".main_slide .slick-current");
+    current.addClass("on").siblings().removeClass("on");
 
-    $('.main_visual .menu li').eq(0).addClass('on');
-    $('.main_visual .menu li').eq(c).addClass('on')
-      .siblings().removeClass('on');
+    $(".main_visual .menu li").eq(0).addClass("on");
+    $(".main_visual .menu li")
+      .eq(c)
+      .addClass("on")
+      .siblings()
+      .removeClass("on");
   });
 
   // $(".con_slide").slick({
@@ -29,13 +32,9 @@ $(function () {
   // });
 
   $(".con_slide").slick({
-    dots: true,
-    draggable: false,
-    arrows: true,
-    adaptiveHeight: true,
+    // dots: true,
+    arrows: false,
     centerMode: true,
-    centerPadding: '350px',
+    centerPadding: "300px",
   });
-
-
-})
+});
