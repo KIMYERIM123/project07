b399d48d844a3c6abbfef9a4799f2eb40eccb37a
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-06-23 14:19:56 +0900

    Add files via upload

--- js/project07.js
+++ js/project07.js
@@ -1,15 +1,4 @@
 $(function () {
-  $(".main_slide").slick({
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3500,
-  });
-
-  $(".main_visual .menu li").on("click", function (e) {
-    e.preventDefault();
-    const idx = $(this).index(); //
-    $(".main_slide").slick("slickGoTo", idx);
-  });
 
   $(".main_slide").on("init afterChange", function (e, s, c) {
     const current = $(".main_slide .slick-current");
@@ -23,13 +12,17 @@ $(function () {
       .removeClass("on");
   });
 
-  // $(".con_slide").slick({
-  //   arrows: false,
-  //   centerMode: true,
-  //   centerPadding: '100px',
-  //   slidesToShow: 1,
-  //   // variableWidth: true
-  // });
+  $(".main_slide").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3500,
+  });
+
+  $(".main_visual .menu li").on("click", function (e) {
+    e.preventDefault();
+    const idx = $(this).index(); //
+    $(".main_slide").slick("slickGoTo", idx);
+  });
 
   $(".con_slide").slick({
     dots: true,
@@ -45,13 +38,19 @@ $(function () {
     $(".con_slide").slick("slickNext");
   });
 
-
   $('.main_discover .tab_menu li').on('click', function () {
     const idx = $(this).index();
     $('.main_discover .tab_con li').eq(idx).addClass('on').siblings().removeClass('on')
     $(this).addClass("on").siblings().removeClass("on");
   })
 
+  $('.to_top').on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 600)
+  });
 
+  $(window).on('scroll', function () {
+    let sct = $(window).scrollTop();
+    sct > 100 ? $('.to_top').addClass('on') : $('.to_top').removeClass('on');
+  });
 
 });
